e94ffb909028b4a7c44c6b5cdc3f93cc15d9dc24
Lei <walei@umich.edu>
2018-06-28 02:46:49 +0000

    add mongolab

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://lei:123abc@ds121341.mlab.com:21341/yelpcamp_lei");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
