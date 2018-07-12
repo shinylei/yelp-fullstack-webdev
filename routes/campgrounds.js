477dfdc8b0a836591383bc07ac26329be3ba25c8
Lei <walei@umich.edu>
2018-07-12 06:33:56 +0000

    add image uploading

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,32 @@ var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
 var middleware = require("../middleware");
+
+//=========================================
+//settings for uploading files
+var multer = require('multer');
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'dlehxaepu', 
+  api_key: '754166444915815', 
+  api_secret: 'exFqHHhjrd5CdeDkw-zm1RhJXZk'
+});
+//=========================================
+
 router.get("/campgrounds", function(req, res) {
      //console.log(req);
    Campground.find({}, function(err, allCampgrounds){
@@ -13,24 +39,43 @@ router.get("/campgrounds", function(req, res) {
    });
 });
 
-router.post("/campgrounds", middleware.isLoggedIn, function(req, res){
-    //get data from form and add to campgrounds array
-    var name = req.body.name;
-    var price = req.body.price;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var author = {
-        id: req.user._id,
-        username: req.user.username
-    };
-    var newCamp = {name: name, price: price, image: image, description: desc, author: author};
-    Campground.create(newCamp, function(err, newlyCreated){
-        if(err) {
-            console.log(err);
-        } else {
-            req.flash("success", "campground created!");
-            res.redirect("/campgrounds");
+router.post("/campgrounds", middleware.isLoggedIn, upload.single('image'), function(req, res){
+    // //get data from form and add to campgrounds array
+    // var name = req.body.name;
+    // var price = req.body.price;
+    // var desc = req.body.description;
+    // var image = req.body.image;
+    // var author = {
+    //     id: req.user._id,
+    //     username: req.user.username
+    // };
+    
+    // var newCamp = {name: name, price: price, image: image, description: desc, author: author};
+    // Campground.create(newCamp, function(err, newlyCreated){
+    //     if(err) {
+    //         console.log(err);
+    //     } else {
+    //         req.flash("success", "campground created!");
+    //         res.redirect("/campgrounds");
+    //     }
+    // });
+    
+    
+    cloudinary.uploader.upload(req.file.path, function(result) {
+        // add cloudinary url for the image to the campground object under image property
+        req.body.campground.image = result.secure_url;
+        // add author to campground
+        req.body.campground.author = {
+            id: req.user._id,
+            username: req.user.username
         }
+        Campground.create(req.body.campground, function(err, campground) {
+            if (err) {
+                req.flash('error', err.message);
+                return res.redirect('back');
+            }
+            res.redirect('/campgrounds/' + campground.id);
+        });
     });
 });
 
